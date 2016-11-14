79eb920fddfd911f200d5f66d579d954a6ab63ca
vincentl2189 <georgeb32@gmail.com>
2016-11-14 11:33:34 -0500

    Update ChassisConversion.ps1

--- ChassisConversion.ps1
+++ ChassisConversion.ps1
@@ -3,23 +3,23 @@
 
 #Hash table
 $ChassisCoversion = @{
-    "3" = "Desktop";
-    "4" = "Desktop";
-    "5" = "Desktop";
-    "6" = "Desktop";
-    "7" = "Desktop";
-    "8" = "Laptop";
-    "9" = "Laptop";
-    "10" = "Laptop";
+    "3" = "Desktop"
+    "4" = "Desktop"
+    "5" = "Desktop"
+    "6" = "Desktop"
+    "7" = "Desktop"
+    "8" = "Laptop"
+    "9" = "Laptop"
+    "10" = "Laptop"
     "11" = "Laptop"
 }
 
 #Import CSV and convert values
-$csv = Import-Csv -Path "\\Path";
+$csv = Import-Csv -Path "\\Path"
 foreach($row in $csv) 
 {
-    $row.Chassis = $ChassisCoversion[$row.Chassis];
+    $row.Chassis = $ChassisCoversion[$row.Chassis]
 }
 
 #Export CSV with new values, to new location
-$csv | Export-Csv -Path "\\PathConverted" -NoTypeInformation;
+$csv | Export-Csv -Path "\\PathConverted" -NoTypeInformation
