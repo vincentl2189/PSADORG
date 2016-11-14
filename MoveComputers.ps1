0c8706da607d8b6a46b382460a9a1caa5cdbb064
vincentl2189 <georgeb32@gmail.com>
2016-11-14 11:18:37 -0500

    Create MoveComputers.ps1

---
+++ MoveComputers.ps1
@@ -0,0 +1,13 @@
+#Move computers based on matching Description and location to specific OU
+Import-Module ActiveDirectory
+
+#Variables
+#All the OU's that computers can be moved to
+    $OU1 = "OU=Example,DC=Domain,DC=com"
+    $OU2 = "OU=Example,DC=Domain,DC=com"
+
+    
+#Functions
+#Computers are moved based on Matching Description and Location
+ Get-ADComputer -Filter {(Description -like "Information Technology Laptop") -and (Location -like "New York")} -ResultSetSize $null| Move-ADObject -TargetPath $OU1
+ Get-ADComputer -Filter {(Description -like "Information Technology Laptop") -and (Location -like "Tokyo")} -ResultSetSize $null| Move-ADObject -TargetPath $OU2
