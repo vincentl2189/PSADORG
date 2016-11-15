6ad2c4145829ea2dad43d42fcbecccb94c28ed60
vincentl2189 <georgeb32@gmail.com>
2016-11-15 16:21:29 -0500

    Professional Readme

--- MoveComputers.ps1
+++ MoveComputers.ps1
@@ -1,3 +1,32 @@
+<# MoveComputers.ps1
+.SYNOPSIS
+	Moves computers in Active Directory
+.DESCRIPTION
+	This script moves computers the their respective OU based on matching criteria
+	Make sure to edit and change the PARAM section to match your environment
+.PARAMETER OU
+	Set the OU you want to move matching computers into
+.PARAMETER DESCRIPTION
+	Set the desciption you used in previous scripts
+.PARAMETER LOCATION
+	Set the location if desired, additional matching is not required
+
+.OUTPUTS
+	CSV:	Exports the converted table into a CSV
+.EXAMPLE
+	.\MoveComputers.ps1
+	
+	
+.NOTES
+	Script:				MoveComputers.ps1
+	Author:				Berry George
+	
+	Changelog
+		1.0				Initial Version
+.LINK
+	Source:			https://github.com/vincentl2189/PSADORG	
+	#>
+    
 #Move computers based on matching Description and location to specific OU
 Import-Module ActiveDirectory
 
